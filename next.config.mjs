f5fa8d07f04271deade92edbd7f89f783c5f1647
Slavouz <tmbwpva@protonmail.com>
2024-05-26 19:33:29 +0700

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/nextjs-github-pages",
     images:{
-        unoptimized: true,
-    },
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
